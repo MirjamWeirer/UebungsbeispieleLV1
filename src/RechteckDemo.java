7efc8e4d6cb2d737f46390db771f73a03027ce36
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-06 13:41:43 +0100

    Uebung02

--- src/RechteckDemo.java
+++ src/RechteckDemo.java
@@ -1,10 +1,13 @@
 public class RechteckDemo {
     public static void main(String[] args) {
+        //Rechteck: Variablen für Länge und breite; Variablen ausgeben
         int breite = 4, laenge = 7;
         System.out.println("Die Länge des Rechecks beträgt: " + laenge + " cm und die Breite beträgt: " + breite + " cm.");
+        //Umfang berechnen
         int umfang;
         umfang = (laenge + breite) * 2;
         System.out.println("Der Umfang des Recktecks beträgt: " + umfang + " cm");
+        //Fläche berechnen
         int flaeche = laenge * breite;
         System.out.println("Die Fläche beträgt: " + flaeche + " cm²");
     }
--- src/VariableDemo.java
+++ src/VariableDemo.java
@@ -1,5 +1,6 @@
 public class VariableDemo {
     public static void main(String[] args) {
+        //zwei Variablen deklarieren für Vor- und Nachname + Ausgabe
         String vorname = "Max";
         String nachname = "Mustermann";
         System.out.println("Der Name lautet: " + vorname + " " + nachname);
